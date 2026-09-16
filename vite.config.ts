ff021650606c207549729ec6dd3054dcf1c4b7d9
unknown <harikrishnan546.07.@gmail.com>
2026-09-16 19:43:16 +0530

    Fix Vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Resume_builder/',
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/Resume_builder/' : '/',
   plugins: [react(), tailwindcss()],
 })
